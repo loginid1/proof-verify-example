41a13d0e093e456e8976233dd2e2eb4410b104c5
braagaa <michael_braga@outlook.com>
2021-10-05 10:16:01 -0400

    heroku ready

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,10 @@
-import express from "express";
+import express, { Response } from "express";
 import cookieParser from "cookie-parser";
+import path from "path";
 import { tokenRouter, identityRouter, userRouter } from "./routes/";
 
 const app = express();
+const buildDir = path.join(__dirname, "..", "build");
 
 app.set("port", process.env.PORT || 3001);
 app.set("env", process.env.NODE_ENV || "development");
@@ -10,10 +12,15 @@ app.set("env", process.env.NODE_ENV || "development");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser("secret"));
+app.use(express.static(buildDir));
 
 app.use("/user", userRouter);
 app.use("/token", tokenRouter);
 app.use("/identity", identityRouter);
+
+app.use((_, res: Response) => {
+  res.sendFile(path.join(buildDir, "index.html"));
+});
 
 app.listen(app.get("port"), () => {
   console.log(`Listening to ${app.get("port")}`);
